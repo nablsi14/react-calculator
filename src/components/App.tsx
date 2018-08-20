d53a71a7c53b9d5230b9c3b04302f4f5c741dea2
Nathaniel Sigafoos <nathaniel.b.sigafoos@gmail.com>
2018-08-20 17:09:16 -0400

    Added CssBaseline as a starting point for styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,23 @@
-import { Grid } from '@material-ui/core'
-import React from 'react'
+import { CssBaseline, Grid } from '@material-ui/core'
+import React, { Fragment } from 'react'
 import Calculator from './calculator/Calculator'
 import { standard } from './calculator/Operations'
 
 const App = () => (
-    <Grid
-        container={true}
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-    >
-        <Grid item={true} xs={12}>
-            <Calculator operations={standard} />
+    <Fragment>
+        <CssBaseline />
+        <Grid
+            container={true}
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+        >
+            <Grid item={true} xs={12}>
+                <Calculator operations={standard} />
+            </Grid>
         </Grid>
-    </Grid>
+    </Fragment>
 )
 
 export default App
