615bdaaa78f89f9c38bddb6cd47cba7eace3a855
Nathaniel Sigafoos <nathaniel.b.sigafoos@gmail.com>
2018-08-27 23:34:51 -0400

    Added a 'precedence' property to the Operation interface

--- src/components/calculator/Operations.tsx
+++ src/components/calculator/Operations.tsx
@@ -1,25 +1,32 @@
+export type Parenthesis = '(' | ')'
+
 export type EvalFunc = (left: number, right: number) => number
 
 export interface Operation {
     evaluate: EvalFunc
+    precedence: number
     text: string
 }
 
 export const standard: Operation[] = [
     {
         evaluate: (left: number, right: number) => left + right,
+        precedence: 1,
         text: '+'
     },
     {
         evaluate: (left: number, right: number) => right - left,
+        precedence: 1,
         text: '−'
     },
     {
         evaluate: (left: number, right: number) => right / left,
+        precedence: 2,
         text: '÷'
     },
     {
         evaluate: (left: number, right: number) => right * left,
+        precedence: 2,
         text: '×'
     }
 ]
