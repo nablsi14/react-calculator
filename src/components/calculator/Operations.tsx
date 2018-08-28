6b77a148c57d7453f0a2a88c42530604015269af
Nathaniel Sigafoos <nathaniel.b.sigafoos@gmail.com>
2018-08-28 16:00:01 -0400

    Added 'shortcut' property to Operation

--- src/components/calculator/Operations.tsx
+++ src/components/calculator/Operations.tsx
@@ -2,9 +2,15 @@ export type Parenthesis = '(' | ')'
 
 export type EvalFunc = (left: number, right: number) => number
 
+interface KeyboardShortcut {
+    alt: boolean
+    ctrl: boolean
+    key: string
+}
 export interface Operation {
     evaluate: EvalFunc
     precedence: number
+    shortcut?: KeyboardShortcut
     text: string
 }
 
@@ -12,21 +18,25 @@ export const standard: Operation[] = [
     {
         evaluate: (left: number, right: number) => left + right,
         precedence: 1,
+        shortcut: { alt: false, ctrl: false, key: '+' },
         text: '+'
     },
     {
         evaluate: (left: number, right: number) => right - left,
         precedence: 1,
+        shortcut: { alt: false, ctrl: false, key: '-' },
         text: '−'
     },
     {
         evaluate: (left: number, right: number) => right / left,
         precedence: 2,
+        shortcut: { alt: false, ctrl: false, key: '/' },
         text: '÷'
     },
     {
         evaluate: (left: number, right: number) => right * left,
         precedence: 2,
+        shortcut: { alt: false, ctrl: false, key: '*' },
         text: '×'
     }
 ]
